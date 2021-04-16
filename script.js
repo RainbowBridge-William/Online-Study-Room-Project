cc7bda029a60f329559a81d9196e4cd2434aa0fe
William <swang45@uw.edu>
2021-04-17 03:25:43 +0800

    login js added

--- script.js
+++ script.js
@@ -5,30 +5,29 @@ function validation() {
 	if (email.match(/\S+@\S+/)) {
 		console.log("good e")
 		document.getElementById("email").className = "";
-		document.getElementById("ewarn").style.dispaly = "";
+		document.getElementById("ewarn").style.display = "";
 		if (password.length > 8) {
 			console.log("good p")
-			document.getElementById("pwarn").style.dispaly = "";
+			document.getElementById("pwarn").style.display = "";
 			document.getElementById("password").className = "";
-			location.href = "./studyroom.html"
       return true;
 		} else {
-			document.getElementById("pwarn").style.dispaly = "block";
-			document.getElementById("password").className = "incorrent";
+			document.getElementById("pwarn").style.display = "block";
+			document.getElementById("password").className = "incorrect";
       return false;
 		}
 	} else {
 		console.log("bad e")
-		document.getElementById("ewarn").style.dispaly = "block";
+		document.getElementById("ewarn").style.display = "block";
 		document.getElementById("email").className = "incorrect";
 		if (password.length > 8) {
-			document.getElementById("pwarn").style.dispaly = "";
+			document.getElementById("pwarn").style.display = "";
 			document.getElementById("password").className = "";
             return false;
 		} else {
 			console.log("bad p")
-			document.getElementById("pwarn").style.dispaly = "block";
-			document.getElementById("password").className = "incorrent";
+			document.getElementById("pwarn").style.display = "block";
+			document.getElementById("password").className = "incorrect";
             return false;
 		}
 	}
